3c63e998160a6ec333465db7e33c2b4039a841b1
Alexander Brefeld <alexander.brefeld@protonmail.com>
2024-11-10 15:54:05 -0600

    switch to postgres

--- bookjournal/settings.py
+++ bookjournal/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = "bookjournal.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+                "options": "-c search_path=bookjournal"
+            },
+        "NAME": "bookjournalxdb",
+        "USER": "journal_manager",
+        "PASSWORD": "ChangeThisPwBfProd!",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
---
+++ resources/setupdb.sql
@@ -0,0 +1,3 @@
+alter role journal_manager set client_encoding to 'utf8';
+alter role journal_manager set default_transaction_isolation to 'read committed';
+alter role journal_manager set timezone to 'America/Chicago';
