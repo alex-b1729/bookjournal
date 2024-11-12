26c2076131e32b66ca9c6479f9bf6411b36e1e27
Alexander Brefeld <alexander.brefeld@protonmail.com>
2024-11-12 10:52:46 -0600

    add Follower and FollowRequest models

--- journal/models/journal.py
+++ journal/models/journal.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.conf import settings
 from django.utils import timezone
+from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 
 from taggit.managers import TaggableManager
@@ -101,3 +102,115 @@ class Profile(models.Model):
     about = models.TextField(
         blank=True,
     )
+
+
+class Follower(models.Model):
+    user_from = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='follower_from_set',
+        on_delete=models.CASCADE,
+    )
+    user_to = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='follower_to_set',
+        on_delete=models.CASCADE,
+    )
+    created = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    class Meta:
+        ordering = ('user_to__username',)
+
+    def __str__(self):
+        return f'{self.user_from.username} follows {self.user_to.username}'
+
+
+User.add_to_class(
+    'following',
+    models.ManyToManyField(
+        'self',
+        through=Follower,
+        symmetrical=True,
+    )
+)
+
+
+class RequestStatus(models.TextChoices):
+    OUTSTANDING = 'o', _('Outstanding')
+    ACCEPTED = 'a', _('Accepted')
+    DECLINED = 'd', _('Declined')
+
+
+class OutstandingManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=RequestStatus.OUTSTANDING)
+
+
+class AcceptedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=RequestStatus.ACCEPTED)
+
+
+class DeclinedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=RequestStatus.DECLINED)
+
+
+class FollowRequest(models.Model):
+    user_from = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='request_from_set',
+        on_delete=models.CASCADE,
+    )
+    user_to = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='request_to_set',
+        on_delete=models.CASCADE,
+    )
+    message = models.CharField(
+        max_length=200,
+        blank=True,
+    )
+    requested = models.DateTimeField(
+        auto_now_add=True,
+    )
+    status = models.CharField(
+        max_length=1,
+        choices=RequestStatus,
+        default=RequestStatus.OUTSTANDING,
+    )
+    updated = models.DateTimeField(
+        auto_now=True,
+    )
+
+    objects = models.Manager()
+    outstanding = OutstandingManager()
+    accepted = AcceptedManager()
+    declined = DeclinedManager()
+
+    class Meta:
+        ordering = ('-requested',)
+
+    def __str__(self):
+        return f'{self.user_from.username} requested to follow {self.user_to.username}'
+
+    def accept(self):
+        self.status = RequestStatus.ACCEPTED
+        self.save()
+        self.user_from.followers.add(self.user_to)
+
+    def decline(self):
+        self.status = RequestStatus.DECLINED
+        self.save()
+
+
+User.add_to_class(
+    'requests_made',
+    models.ManyToManyField(
+        'self',
+        through=FollowRequest,
+        related_name='requests_of',
+        symmetrical=False,
+    )
+)
